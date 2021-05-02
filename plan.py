297f2dd8d44b7be32b9e6eaf356d9aa4c86c6bf8
freercurse <freerjoseph@yahoo.co.yk>
2021-05-02 21:26:31 +0100

    modified the environment to suit a cat and mouse game, steering behaviours is unmodified currently.

--- plan.py
+++
@@ -1,6 +0,0 @@
-# DONE define a fish so i can call update on it and pass in a bunch of inputs and it'll update its pos
-# DONE manage the core game loop so it'll draw everything
-# DONE wander
-# DONE Flock Centering: attempt to stay close to nearby flock mates
-# todo: make the shark
-# todo: run from the shark
